c99d4dcb937ba5447fe5be4dbbf398fa39293dd4
saliha <112631317+salihameryemalbayrak@users.noreply.github.com>
2025-11-27 17:33:59 +0300

    5. videonun 30. dakikasina kadar

---
+++ diginova-opencv/components/GrayTest/src/executors/GrayTest.py
@@ -0,0 +1,34 @@
+"""
+    It is one of the preprocessing components in which the image is rotated.
+"""
+
+import os
+import cv2
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
+
+from sdks.novavision.src.media.image import Image
+from sdks.novavision.src.base.component import Component
+from sdks.novavision.src.helper.executor import Executor
+from components.Package.src.utils.response import build_response
+from components.Package.src.models.PackageModel import PackageModel
+
+
+class Package(Component):
+    def __init__(self, request, bootstrap):
+        super().__init__(request, bootstrap)
+        self.request.model = PackageModel(**(self.request.data))
+
+    @staticmethod
+    def bootstrap(config: dict) -> dict:
+        return {}
+
+
+
+    def run(self):
+        pass
+
+
+if "__main__" == __name__:
+    Executor(sys.argv[1]).run()
